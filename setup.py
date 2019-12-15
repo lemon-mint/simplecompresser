6ca25083c6099679cc930e1dde83288b507f9313
lemon-mint <ice.lemons.mint@gmail.com>
2019-12-15 19:27:25 +0900

    ready to deploy

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'simplecodecompresser',
+    version          = '1.0',
+    description      = 'Just a simple code compresser',
+    author           = 'lemon-mint',
+    author_email     = 'ice.lemons.mint@gmail.com',
+    url              = 'https://github.com/lemon-mint/simplecompresser',
+    download_url     = 'https://github.com/lemon-mint/simplecompresser',
+    install_requires = [],
+    packages         = find_packages(),
+    keywords         = ['compress', 'codedeploy','deploy'],
+    python_requires  = '>=3',
+    package_data     =  {},
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
+)

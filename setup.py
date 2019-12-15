6564fe5c4ee50e6a7f1c1fef4871494fbc9e2d2e
lemon-mint <ice.lemons.mint@gmail.com>
2019-12-15 22:01:36 +0900

    build fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'SimpleCodeCompresser',
-    version          = '1.0',
+    version          = '1.1',
     description      = 'Just a simple code compresser',
     author           = 'lemon-mint',
     author_email     = 'ice.lemons.mint@gmail.com',
